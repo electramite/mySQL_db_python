b70c7ef69a1264e56c67d330167d6a1632236601
shashwat <electramite@gmail.com>
2021-03-07 00:32:23 +0530

    Create 2_make_Database.py

---
+++ 2_make_Database.py
@@ -0,0 +1,8 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="yourpassword"
+)
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE sensor_data")
